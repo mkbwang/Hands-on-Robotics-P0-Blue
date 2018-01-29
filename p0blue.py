7b0c3d83a9067632e23d4fe6952ddbcc8704a08e
Ali Yassine <yassinea@umich.edu>
2018-01-29 12:45:30 -0500

    Final version commented

--- p0blue.py
+++ p0blue.py
@@ -1,80 +1,90 @@
 from joy import *
 
-#class ShaveNHaircutPlan( Plan ):
-  #"""
-  #ShaveNHaircutPlan shows a simple example of sequential composition:
-  #its behavior is to run the shave plan followed by the haircut plan.
-  #"""
-#  def __init__(self,app,shave,haircut,tail,*arg,**kw):
-#    Plan.__init__(self,app,*arg,**kw)
-#    self.shave = shave
-#    self.haircut = haircut
-#    self.tail = tail
-    
-#  def behavior( self ):
-#    progress("Both: starting 'Shave' sequence")
-#    yield self.shave
-#    progress("Both: starting 'Haircut' sequence")
-#    yield self.haircut    
- #   progress("Both: starting 'Tail' sequence")
- #   yield self.tail    
- #   progress("Both: done")
+#@title: Blue Team Project Zero Code
+#@authors: Mukai Wang, Ali Yassine, Anne Gu, Royce Chung
+#@date: 1/29/2018
+#@course: EECS 464 - Hands On Robotics
+#
+#Derived from shave and haircut demo
+#Redesigned to working with 2018 Project 0 Blue Team Leaping robot
+#Key terminology
+#plan: method of performing function i.e. turning left, right, or moving foward
+#leftPlan: method to turn left
+#rightPlan: method to turn right
+#leapPlan: method to leap foward
+#onStart(): Overided method from JoyApp class that starts the code by loading the plans
+#onEvent(): Overided method from JoyApp class that reads keyboard commands to run plans
 
-    
-class ShaveNHaircutApp( JoyApp ):
-  # Load both patterns from their CSV files
+
+class ProjectZeroApp( JoyApp ):
+  # Load left, right, and leap motion patterns from their CSV files
     LEFT = loadCSV("left.csv")
     RIGHT = loadCSV("right.csv")
     LEAP = loadCSV("leap.csv")
 
-    def __init__(self,shaveSpec,hairSpec,tailSpec,*arg,**kw):
+  #Constructor for ProjectZeroApp. Initializes JoyApp instance	
+    def __init__(self,rightSpec,leftSpec,leapSpec,*arg,**kw):
         JoyApp.__init__(self, *arg,**kw)
-        self.shaveSpec = shaveSpec
-        self.hairSpec = hairSpec
-        self.tailSpec = tailSpec
+        self.rightSpec = rightSpec
+        self.leftSpec = leftSpec
+        self.leapSpec = leapSpec
 
+  #Overide for onStart() function in JoyApp
+  #Reads motion patterns and sorts plans into their appropriate plans
     def onStart(self):
-    
-        self.leapplan = SheetPlan(self, self.LEAP, x=self.tailSpec, y=self.shaveSpec, z=self.hairSpec)
-        self.leftplan = SheetPlan(self, self.LEFT, x=self.tailSpec, y=self.shaveSpec, z=self.hairSpec)
-        self.rightplan = SheetPlan(self, self.RIGHT, x=self.tailSpec, y=self.shaveSpec, z=self.hairSpec)
+  
+  #Sorting done here  
+        self.leapplan = SheetPlan(self, self.LEAP, x=self.leapSpec, y=self.rightSpec, z=self.leftSpec)
+        self.leftplan = SheetPlan(self, self.LEFT, x=self.leapSpec, y=self.rightSpec, z=self.leftSpec)
+        self.rightplan = SheetPlan(self, self.RIGHT, x=self.leapSpec, y=self.rightSpec, z=self.leftSpec)
 
-        # give us start and stop messages; in your own code you can omit these 
+  #Shows progress messages when a motion is called
+  #Useful for debugging
         self.leapplan.onStart = lambda : progress("Leap: starting") 
         self.leapplan.onStop = lambda : progress("Leap: done")
         self.leftplan.onStart = lambda : progress("Left: starting")
         self.leftplan.onStop = lambda : progress("Left: done")
         self.rightplan.onStart = lambda : progress("Right: starting")
         self.rightplan.onStop = lambda : progress("Right: done") 
-        #
-        # Set up a ShaveNHaircutPlan using both of the previous plans
-        #
-        #self.both = ShaveNHaircutPlan(self, self.shaveplan, self.hairplan,self.tailplan)
-
+ 
+  #Overide for onEvent() function in JoyApp
+  #Reads input from keyboard and runs plans based off what key was pressed
+  #Can terminated program in case of emergency
     def onEvent(self,evt):
         if evt.type != KEYDOWN:
             return
-    # assertion: must be a KEYDOWN event 
+  # Assertion: must be a KEYDOWN event 
+    
+  #If 'W' key is pressed down, run the leap plan
         elif evt.key == K_w:
             if ( not self.leapplan.isRunning() 
                 and not self.leftplan.isRunning()
                 and not self.rightplan.isRunning()
                 ):
                 self.leapplan.start()
+
+  #If 'A' key is pressed down, run the left plan
         elif evt.key == K_a:
             if ( not self.leapplan.isRunning() 
                 and not self.leftplan.isRunning()
                 and not self.rightplan.isRunning()
                 ):
                 self.leftplan.start()
+
+  #If 'D' key is pressed down, run the right plan
         elif evt.key == K_d:
             if ( not self.leapplan.isRunning() 
                 and not self.leftplan.isRunning()
                 and not self.rightplan.isRunning()
                 ):
                 self.rightplan.start()
+  #If the escape key is pressed down, end the program
+  #Overides all other commands, useful emergency switch
         elif evt.key == K_ESCAPE:
             self.stop()
+
+  #If no keys are being pressed, keep the program running,
+  #but stop all running plans
         else:
             if self.leapplan.isRunning():
                 self.leapplan.stop()
@@ -94,34 +104,35 @@ if __name__=="__main__":
         if arg=='--mod-count' or arg=='-c':
             N = int(args.pop(0))
             robot = dict(count=N)
-        elif arg=='--tail' or arg=='-t':
-            shaveSpec = args.pop(0)
-            hairSpec = args.pop(0)
-            tailSpec = args.pop(0)
-            if tailSpec[:1]==">": scr = {}
-
+        elif arg=='--assign-servo' or arg=='-s':
+            rightSpec = args.pop(0)
+            leftSpec = args.pop(0)
+            leapSpec = args.pop(0)
+            if leapSpec[:1]==">": scr = {}
         elif arg=='--help' or arg=='-h':
             sys.stdout.write("""
                 Usage: %s [options]
                 This is used for team blue in P0
-                When running, the demo uses the keyboard. The keys are:
-                'l' -- turn left
-                'r' -- turn right
-                'b' -- leap forward
-                'z' -- stop the current motion, if any
+                When running, the app uses the keyboard. The keys are:
+                'a' -- turn left
+                's' -- turn right
+                'w' -- leap forward
+                no key pressed -- stop the current motion, if any
                 'escape' -- exit program
                 Options:      
                     --mod-count <number> | -c <number>
                     Search for specified number of modules at startup
-                    --shave <spec> | -s <spec>
-                    --haircut <spec> | -h <spec>
-                    Specify the output setter to use for 'shave' (resp. 'haircut')
+                    --assign-servo <spec> | -s <spec>
+                    Specify which servo performs which functions.
+		    In order of right, left, and leap servo on robot
                     Typical <spec> values would be:
-                    'Nx3C/@set_pos' -- send to position of CKBot servo module with ID 0x3C
+                    'Nx0C/@set_pos Nx65/@set_pos Nx02/@set_pos' 
+                    Where Nx0C is the right module, Nx65 is the left module, 
+                    and  Nx02 is the leap Module
                     NOTE: to use robot modules you MUST also specify a -c option
                     """ % sys.argv[0])
             sys.exit(1)
     # ENDS cmdline parsing loop
   
-    app = ShaveNHaircutApp(shaveSpec,hairSpec,tailSpec,robot=robot,scr=scr)
+    app = ProjectZeroApp(rightSpec,leftSpec,leapSpec,robot=robot,scr=scr)
     app.run()
